eb981ab464d86cb84bb769015c0a649828e0fb67
JohnnyWang1998 <johnny98@foxmail.com>
2022-04-14 17:05:21 -0400

    change folders

---
+++ ukraine-russia-conflict-tweets-nlp-project/ukraine-russia-conflict-tweets-nlp-project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ukraine and Russia Conflict Tweets Sentiment Analysis and Topic Modeling',
+    author='group22',
+    license='MIT',
+)
